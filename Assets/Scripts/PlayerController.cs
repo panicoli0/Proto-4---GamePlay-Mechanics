d2464b30bdba91e794fe660961cecb3cb0ee44d4
Pablo Nicoli <pablo.nicoli@hotmail.com>
2021-01-25 16:50:55 -0300

    New Funcionality
    When the player collects a powerup, a visual indicator appears
    When the player collides with an enemy while they have the powerup, the enemy goes flying
    After a certain amount of time, the powerup ability and indicator disappear

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,10 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public GameObject powerUpIndicator;
+    public bool hasPowerup;
+
+    private float powerupStrength = 15.0f;
 
 
     // Start is called before the first frame update
@@ -21,5 +25,36 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+        powerUpIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0); // Cuando agarras el powerUp que el indicator siga al Player
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+            if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpConuntdownRoutine());
+            powerUpIndicator.gameObject.SetActive(true);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if  (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            
+            Debug.Log("Collide with: " + collision.gameObject.name + "with a power up set to" + hasPowerup);
+
+            enemyRb.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
+
+    }
+    IEnumerator PowerUpConuntdownRoutine()
+    {
+        yield return new WaitForSeconds(5); //Updatea una funcion fuera de Update()
+        hasPowerup = false;
+        powerUpIndicator.gameObject.SetActive(false);
     }
 }
